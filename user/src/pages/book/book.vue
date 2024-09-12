4994e291cb2b460bcfa8049fee6ad2261566e7b0
xiao-ding1 <2294778111@qq.com>
2024-09-12 21:06:06 +0800

    修改

--- user/src/pages/book/book.vue
+++ user/src/pages/book/book.vue
@@ -17,12 +17,16 @@
             v-for="(slot, index) in filteredTimeSlots"
             :key="slot.timeId"
             class="time-slot"
-            :class="{ selected: currentReservationTimeSolt === slot.timeId}"
             @click="reserveSlot(index)"
           >
-            <text>{{ slot.startTime }} - {{ slot.endTime }}</text>
+              <image
+                src="../../static/预约1.png"
+                class="icon"/>
+            <view class="time-info">
+              <text>预约时间：{{ formatTime(slot.startTime) }} - {{ formatTime(slot.endTime) }}</text>
+            <text class="capacity-info">预约人数：{{ slot.capacity - slot.remaining }}/{{ slot.capacity }}</text>
             <text v-if="currentReservationTimeSolt === slot.timeId" class="reserved-text">已预约</text>
-            <text>{{ slot.capacity - slot.remaining }}/{{ slot.capacity }}</text>
+            </view>
           </view>
         </view>
       </view>
@@ -38,7 +42,7 @@ export default {
       timeSlots: [], // 请求的时间段
       filteredTimeSlots: [], // 过滤后的时间段
       showCalendar: false,
-       stageTitle: uni.getStorageSync('currentStageName'),
+      stageTitle: uni.getStorageSync('currentStageName'),
       info: {
         lunar: true,
         range: true,
@@ -47,41 +51,54 @@ export default {
       },
       availableDates: [], // 可预约日期
       selectedDate: "",
-      currentReservationId:uni.getStorageSync('reservation') ,
-      currentReservationTimeSolt:uni.getStorageSync('currentReservationTimeSolt')
+      currentReservationId: uni.getStorageSync('currentReservationId'),
+      currentReservationTimeSolt: uni.getStorageSync('currentReservationTimeSolt')
     }
   },
   onReady() {
     this.$nextTick(() => {
       this.showCalendar = true
-   })
-     this.fetchDetails()
-       .then(details => {
-         const { direction} = details
-         const stageId=uni.getStorageSync('currentStageId')
-         console.log(direction,stageId)
+    })
+    this.fetchDetails()
+      .then(details => {
+        const { direction } = details
+        const stageId = uni.getStorageSync('currentStageId')
+        console.log(direction, stageId)
         this.fetchAppointmentData(direction, stageId)
-       })
-       .catch(error => {
-         console.error('获取方向失败:', error)
-       })
+      })
+      .catch(error => {
+        console.error('获取方向失败:', error)
+      })
     this.info.selected = []
   },
   methods: {
-     fetchDetails() {
-       return http({
-         url: '/user/user',
-         method: 'GET'
-       })
-       .then(response => {
-         return {
-           direction: response.direction,
-         }
-       })
-    .catch(error => {
-        console.error('获取方向失败:', error);
-       })
-     },
+    fetchDetails() {
+      return http({
+        url: '/user/user',
+        method: 'GET'
+      })
+        .then(response => {
+          if (!response) {
+            uni.showModal({
+              title: '提示',
+              content: '您尚未报名，请先报名',
+              success: function (res) {
+                if (res.confirm) {
+                  uni.switchTab({
+                    url: '/pages/myth/myth',
+                  })
+                }
+              }
+            });
+          }
+          return {
+            direction: response.direction,
+          }
+        })
+        .catch(error => {
+          console.error('获取方向失败:', error);
+        })
+    },
     fetchAppointmentData(direction, stageId) {
       uni.showLoading({ title: '正在获取信息...' });
       return http({
@@ -92,25 +109,28 @@ export default {
         },
         method: 'GET'
       })
-      .then(response => {
-        this.timeSlots = response.map(slot => ({
-          ...slot,
-          startTime: slot.startTime.replace('T', ' '),
-          endTime: slot.endTime.replace('T', ' ')
-        }))
-        const uniqueDates = [...new Set(this.timeSlots.map(slot => slot.startTime.split(' ')[0]))]
-        this.availableDates = uniqueDates;
-        this.info.selected = uniqueDates.map(date => ({
-          date: date,
-          info: '可预约'
-        }))
-      })
-      .catch(error => {
-        console.error('获取预约数据失败:', error);
-      })
-      .finally(() => {
-    uni.hideLoading()
-  })
+        .then(response => {
+          this.timeSlots = response.map(slot => ({
+            ...slot,
+            startTime: slot.startTime.replace('T', ' '),
+            endTime: slot.endTime.replace('T', ' ')
+          }))
+          const uniqueDates = [...new Set(this.timeSlots.map(slot => slot.startTime.split(' ')[0]))]
+          this.availableDates = uniqueDates;
+          this.info.selected = uniqueDates.map(date => ({
+            date: date,
+            info: '可预约'
+          }))
+        })
+        .catch(error => {
+          console.error('获取预约数据失败:', error);
+        })
+        .finally(() => {
+          uni.hideLoading()
+        })
+    },
+    formatTime(datetime) {
+      return datetime.split(' ')[1].substring(0, 5)
     },
     handleDateChange(e) {
       const selectedDate = e.fulldate;
@@ -132,31 +152,31 @@ export default {
         url: `/interview/cancel/${reservationId}`,
         method: 'PUT'
       })
-      .then(() => {
-        this.filteredTimeSlots = this.filteredTimeSlots.map(slot => {
-      if (slot.timeId === this.currentReservationTimeSolt) { 
-        return { ...slot, remaining: slot.remaining +1 }
-      }
-      return slot
-    })
-        uni.showToast({
-          title: "预约已取消",
-          icon: "success",
+        .then(() => {
+          this.filteredTimeSlots = this.filteredTimeSlots.map(slot => {
+            if (slot.timeId === this.currentReservationTimeSolt) {
+              return { ...slot, remaining: slot.remaining + 1 }
+            }
+            return slot
+          })
+          this.currentReservationId = null
+          this.currentReservationTimeSolt = null
+          uni.removeStorageSync('currentReservationId')
+          uni.removeStorageSync('currentReservationTimeSolt')
+          uni.showToast({
+            title: "预约已取消",
+            icon: "success",
+          })
         })
-        this.currentReservationId = null
-        this.currentReservationTimeSlot = null
-        uni.removeStorageSync('reservation')
-        uni.removeStorageSync('currentReservationTimeSolt')
-      })
-      .catch(error => {
-        uni.showToast({
-          title: "取消预约失败",
-          icon: "none",
+        .catch(() => {
+          uni.showToast({
+            title: "取消预约失败",
+            icon: "none",
+          })
         })
-      })
-      .finally(() => {
-    uni.hideLoading()
-  });
+        .finally(() => {
+          uni.hideLoading()
+        });
     },
     submitReservation(timeId) {
       uni.showLoading({ title: '正在预约中...' });
@@ -164,54 +184,54 @@ export default {
         url: `/interview/${timeId}`,
         method: 'POST'
       })
-      .then(response => {
-        uni.setStorageSync('reservation', response.reservationId)
-        uni.setStorageSync('currentReservationTimeSolt', timeId)
-        this.currentReservationId = response.reservationId
-        this.currentReservationTimeSlot= uni.getStorageSync(' currentReservationTimeSolt')
-        this.filteredTimeSlots = this.filteredTimeSlots.map(slot => {
-      if (slot.timeId === timeId) {
-        return { ...slot, remaining: slot.remaining - 1 }
-      }
-      return slot
-    })
-        uni.showToast({
-          title: "预约成功",
-          icon: "success",
+        .then(response => {
+          uni.setStorageSync('currentReservationId', response.reservationId)
+          uni.setStorageSync('currentReservationTimeSolt', timeId)
+          this.currentReservationId = response.reservationId
+          this.currentReservationTimeSolt = response.currentReservationId
+          this.filteredTimeSlots = this.filteredTimeSlots.map(slot => {
+            if (slot.timeId === timeId) {
+              return { ...slot, remaining: slot.remaining - 1 }
+            }
+            return slot
+          })
+          uni.showToast({
+            title: "预约成功",
+            icon: "success",
+          })
         })
-      })
-      .catch(error => {
-        console.log(error)
-        uni.showToast({
-          title:error.data.message+'请先取消预约',
-          icon: "none",
+        .catch(error => {
+          console.log(error)
+          uni.showToast({
+              title: error.message + '请先取消预约',
+              icon: "none",
+            })
         })
-      })
-      .finally(() => {
-    uni.hideLoading()
-  })
+        .finally(() => {
+          uni.hideLoading()
+        })
     },
     reserveSlot(index) {
-    const slot = this.filteredTimeSlots[index];
-    if (this.currentReservationId && this.currentReservationTimeSolt === slot.timeId) {
-      // 当前时间段已被预约，进行取消操作
-      this.cancelReservation(this.currentReservationId);
-    } else {
-      // 没有预约，直接进行新的预约
-      if (slot.remaining > 0) {
-        this.submitReservation(slot.timeId)
-          .then(() => {
-            // 设置当前预约的时间段索引
-            this.currentReservationTimeSolt = slot.timeId;
-          });
+      const slot = this.filteredTimeSlots[index];
+      if (this.currentReservationId && this.currentReservationTimeSolt === slot.timeId) {
+        // 当前时间段已被预约，进行取消操作
+        this.cancelReservation(this.currentReservationId);
       } else {
-        uni.showToast({
-          title: "此时间段已满",
-          icon: "none",
-        })
+        // 没有预约，直接进行新的预约
+        if (slot.remaining > 0) {
+          this.submitReservation(slot.timeId)
+            .then(() => {
+              // 设置当前预约的时间段索引
+              this.currentReservationTimeSolt = slot.timeId;
+            });
+        } else {
+          uni.showToast({
+            title: "此时间段已满",
+            icon: "none",
+          })
+        }
       }
     }
-  }
   }
 }
 </script>
@@ -228,42 +248,54 @@ export default {
 
 .button-text {
   background-color: #fff;
-  color: #100f0f;
-  font-size: 12px;
-  height: 350px;
+  color: #323232;
+  font-size: 10px;
+  height: 360px;
   width: 100%;
-  margin-bottom: 0;
   border-top-left-radius: 10px;
   border-top-right-radius: 10px;
 }
 
 .time-slots {
-  margin-top: 10px; 
+  margin: 10px auto;
+  width: 80%
 }
 
 .time-slot {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 10px;
-  margin: 5px 0;
-  background-color: #f5f1f1;
+  padding: 5px;
+  background-color: #d4f8f8;
   border-radius: 5px;
   cursor: pointer;
+  margin-bottom: 10px;
+height: 70px;
+}
+
+.time-info {
+  display: flex;
+  flex-direction: column;
+  font-size: 12px;
+  margin-right: 90px;
+}
+
+.icon {
+  width: 35px;
+  height: 35px;
+  margin-left: 10px;
+}
+.capacity-info {
+  font-size: 12px;
 }
 
 .time-slot:hover {
-  background-color: #eaeaea;
+  background-color: #abdfee;
 }
 
-.time-slot.selected {
-  background-color: #a0e0a0; 
-  color: #fff;
-}
 .reserved-text {
   color: #ff0000;
   font-size: 12px;
   margin-left: 10px;
 }
-
 </style>
